ebfd28ac78bcae62f707889fd05428ef160aa32e
Petya Gavrilova <petya.gavrilova@hashicorp.com>
2023-02-13 23:50:57 +0200

    main.tf updated

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.53.0"
-    }
+provider "aws" {
+  alias  = "west"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "east"
+  region = "us-east-1"
+}
+
+module "keypair-west" {
+  source = "../working-with-modules/modules"
+  providers = { aws = aws.west
+  }
+
+}
+
+module "keypair-east" {
+  source = "../working-with-modules/modules"
+  providers = { aws = aws.east
   }
 }
-
-module "modules" {
-    source = "../working-with-modules/modules"
-}
-
 
 
